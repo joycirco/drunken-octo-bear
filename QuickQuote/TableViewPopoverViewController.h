7e59a555c9518171cc8679685eff5a60769bbc5e
Darin Raffety <darin.raffety@engagedtechnologies.com>
2013-03-21 00:03:30 -0500

    added odfl logo

---
+++ QuickQuote/TableViewPopoverViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewPopoverViewController.h
+//  QuickQuote
+//
+//  Created by Darin Raffety on 3/20/13.
+//  Copyright (c) 2013 EngagedTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewPopoverViewController : UIViewController <UITableViewDelegate>
+
+@end
